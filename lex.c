b81421f7459eb097df20d599962ffdf879d8123f
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-08-09 20:30:22 +0530

    Lexical Analyser and Symbol Table

---
+++ lex.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+void lexicalanalysis(const char *input)
+{
+    int i = 0;
+    while (input[i] != '\0')
+    {
+        while (isspace(input[i]))
+        {
+            i++;
+        }
+        if (isalpha(input[i]) || input[i] == '_')
+        {
+            char buf[1000];
+            int j = 0;
+            while (isalnum(input[i]) || input[i] == '_')
+            {
+                buf[j++] = input[i++];
+            }
+            buf[j] = '\0';
+            if (strcmp(buf, "for") == 0 || strcmp(buf, "int") == 0 || strcmp(buf, "char") == 0 || strcmp(buf, "double") == 0 || strcmp(buf, "return") == 0 || strcmp(buf, "float") == 0 || strcmp(buf, "void") == 0 || strcmp(buf, "continue") == 0 || strcmp(buf, "break") == 0 || strcmp(buf, "goto") == 0 || strcmp(buf, "if") == 0 || strcmp(buf, "register") == 0 || strcmp(buf, "typedef") == 0 || strcmp(buf, "switch") == 0 || strcmp(buf, "do") == 0 || strcmp(buf, "long") == 0 || strcmp(buf, "extern") == 0 || strcmp(buf, "while") == 0 || strcmp(buf, "unsigned") == 0 || strcmp(buf, "short") == 0)
+            {
+                printf("Token - Keyword: %s\n", buf);
+            }
+            else
+            {
+                printf("Token - Identifier: %s\n", buf);
+            }
+        }
+        else if (isdigit(input[i]))
+        {
+            char buf[1000];
+            int j = 0;
+            while (isdigit(input[i]))
+            {
+                buf[j++] = input[i++];
+            }
+            buf[j] = '\0';
+            printf("Token - Number: %s\n", buf);
+        }
+        else
+        {
+            switch (input[i])
+            {
+            case '+':
+            case '-':
+            case '=':
+                if (input[i] == input[i + 1])
+                {
+                    printf("Token - Operator: %c%c\n", input[i], input[i + 1]);
+                    i += 2;
+                }
+                else
+                {
+                    printf("Token - Operator: %c\n", input[i]);
+                    i++;
+                }
+                break;
+            case '<':
+            case '>':
+                if (input[i + 1] == '=')
+                {
+                    printf("Token - Operator: %c=\n", input[i]);
+                    i += 2;
+                }
+                else
+                {
+                    printf("Token - Operator: %c\n", input[i]);
+                    i++;
+                }
+                break;
+            case '*':
+            case '/':
+                printf("Token - Operator: %c\n", input[i]);
+                i++;
+                break;
+            case '(':
+            case ')':
+            case '{':
+            case '}':
+            case ';':
+            case ',':
+            case '&':
+            case ']':
+            case '[':
+                printf("Token - Punctuator: %c\n", input[i]);
+                i++;
+                break;
+            case '"':
+                int q = i + 1;
+                printf("Token - Literal: ");
+                while (input[q] != '"')
+                {
+                    printf("%c", input[q]);
+                    q++;
+                }
+                printf("\n");
+                i = ++q;
+                break;
+            default:
+            }
+        }
+    }
+}
+int main()
+{
+    char input[1000];
+    printf("Enter the input string: ");
+    fgets(input, sizeof(input), stdin);
+    printf("\n========================================================== \n");
+    lexicalanalysis(input);
+    return 0;
+}
---
+++ symtable.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_TOKEN_SIZE 100
+#define MAX_SYMBOLS 100
+
+typedef enum
+{
+    IDENTIFIER,
+    NUMBER,
+    OPERATOR,
+    DATATYPE,
+    KEYWORD,
+    UNKNOWN
+} TokenType;
+typedef struct
+{
+    TokenType type;
+    char value[MAX_TOKEN_SIZE];
+} Token;
+typedef struct
+{
+    char symbol[MAX_TOKEN_SIZE];
+    char type[MAX_TOKEN_SIZE];
+    int size;
+    int location;
+} Symbol;
+
+Symbol symbolTable[MAX_SYMBOLS];
+int symbolCount = 0;
+int currentLocation = 1000;
+
+int stringtoInt(const char *str)
+{
+    int result = 0;
+    while (*str)
+    {
+        if (isdigit(*str))
+        {
+            result = result * 10 + (*str - '0');
+        }
+        str++;
+    }
+    return result;
+}
+
+void addSymbol(char *symbol, char *type, int size)
+{
+    strcpy(symbolTable[symbolCount].symbol, symbol);
+    strcpy(symbolTable[symbolCount].type, type);
+    symbolTable[symbolCount].size = size;
+    symbolTable[symbolCount].location = currentLocation;
+    currentLocation += size;
+    symbolCount++;
+}
+
+void printSymbolTable()
+{
+    printf("\n\n=====================================\n\n");
+    printf("Location\tSymbol\tType\tSize\n");
+    for (int i = 0; i < symbolCount; i++)
+    {
+        printf("%d\t\t%s\t%s\t%d\n", symbolTable[i].location, symbolTable[i].symbol,
+               symbolTable[i].type, symbolTable[i].size);
+    }
+}
+
+int isOperator(char ch)
+{
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' ||
+           ch == ',' || ch == '=' || ch == '<' || ch == '>' ||
+           ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == '[' || ch == ']';
+}
+int isSpecialOperator(const char *str)
+{
+    return strcmp(str, "==") == 0 || strcmp(str, "!=") == 0 ||
+           strcmp(str, "<=") == 0 || strcmp(str, ">=") == 0 ||
+           strcmp(str, "++") == 0 || strcmp(str, "--") == 0;
+}
+int isDatatype(const char *str)
+{
+    return strcmp(str, "int") == 0 || strcmp(str, "float") == 0 ||
+           strcmp(str, "char") == 0 || strcmp(str, "double") == 0 ||
+           strcmp(str, "void") == 0;
+}
+int isKeyword(const char *str)
+{
+    return strcmp(str, "for") == 0 || strcmp(str, "while") == 0 ||
+           strcmp(str, "if") == 0 || strcmp(str, "else") == 0 ||
+           strcmp(str, "return") == 0 || strcmp(str, "break") == 0 ||
+           strcmp(str, "continue") == 0;
+}
+int getSizeOfType(const char *type)
+{
+    if (strcmp(type, "int") == 0)
+        return 2;
+    if (strcmp(type, "float") == 0)
+        return 4;
+    if (strcmp(type, "char") == 0)
+        return 1;
+    if (strcmp(type, "double") == 0)
+        return 8;
+    return 0;
+}
+
+void lexAnalysis(const char *input)
+{
+    int i = 0;
+    int length = strlen(input);
+    char currentType[MAX_TOKEN_SIZE] = "";
+    while (i < length)
+    {
+        Token token;
+        int j = 0;
+        if (isspace(input[i]))
+        {
+            i++;
+            continue;
+        }
+        if (isalpha(input[i]))
+        {
+            while (i < length && (isalpha(input[i]) || isdigit(input[i])))
+            {
+                token.value[j++] = input[i++];
+            }
+            token.value[j] = '\0';
+            if (isDatatype(token.value))
+            {
+                token.type = DATATYPE;
+                strcpy(currentType, token.value);
+            }
+            else if (isKeyword(token.value))
+            {
+                token.type = KEYWORD;
+            }
+            else
+            {
+                token.type = IDENTIFIER;
+                if (currentType[0] != '\0')
+                {
+                    int size = getSizeOfType(currentType);
+                    if (i < length && input[i] == '[')
+                    {
+                        i++;
+                        j = 0;
+                        char sizeStr[MAX_TOKEN_SIZE];
+                        while (i < length && isdigit(input[i]))
+                        {
+                            sizeStr[j++] = input[i++];
+                        }
+                        sizeStr[j] = '\0';
+                        if (i < length && input[i] == ']')
+                        {
+                            i++;
+                            int arraySize = stringtoInt(sizeStr);
+                            size *= arraySize;
+                        }
+                        addSymbol(token.value, strcat(currentType, "*"), size);
+                    }
+                    else
+                    {
+                        addSymbol(token.value, currentType, size);
+                    }
+                }
+                else
+                {
+                    printf("Error: '%s' is not a valid identifier\n", token.value);
+                }
+            }
+        }
+        else if (isdigit(input[i]))
+        {
+            while (i < length && isdigit(input[i]))
+            {
+                token.value[j++] = input[i++];
+            }
+            token.value[j] = '\0';
+            token.type = NUMBER;
+            if (currentType[0] != '\0')
+            {
+                printf("Error: '%s' is not a valid identifier\n", token.value);
+            }
+        }
+        else if (isOperator(input[i]))
+        {
+            token.value[j++] = input[i++];
+            if (i < length && isOperator(input[i]))
+            {
+                token.value[j++] = input[i++];
+                token.value[j] = '\0';
+                if (isSpecialOperator(token.value))
+                {
+                    token.type = OPERATOR;
+                }
+                else
+                {
+                    token.value[--j] = '\0';
+                    i--;
+                    token.type = OPERATOR;
+                }
+            }
+            else
+            {
+                token.value[j] = '\0';
+                token.type = OPERATOR;
+            }
+        }
+        else
+        {
+            token.value[j++] = input[i++];
+            token.value[j] = '\0';
+            token.type = UNKNOWN;
+        }
+    }
+}
+
+int main()
+{
+    char input[MAX_TOKEN_SIZE];
+    printf("Enter the input string: ");
+    fgets(input, MAX_TOKEN_SIZE, stdin);
+    size_t len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n')
+    {
+        input[len - 1] = '\0';
+    }
+    lexAnalysis(input);
+    printSymbolTable();
+    return 0;
+}
